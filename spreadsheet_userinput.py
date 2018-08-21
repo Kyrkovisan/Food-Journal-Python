5a6929d124ad8d48f6c5e1947efc07999a82d99d
Kyrkovisan <38996565+Kyrkovisan@users.noreply.github.com>
2018-08-21 16:20:44 +1000

    this is my first commit
    body of the commit message

---
+++ spreadsheet_userinput.py
@@ -0,0 +1,52 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pprint
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+
+sheet = client.open('food_prep_python').sheet1
+
+#.......................
+#
+#
+#
+# ASK
+# FOR
+# USER
+# INPUT
+#
+#
+#
+# ...................
+
+user_input = input('What did you eat? :')
+ingredients = sheet.col_values(1)
+pp = pprint.PrettyPrinter()
+
+#...........................
+#
+#
+#..........................
+
+
+def check_if_exist(a):
+    count = 1
+    for x in ingredients:
+        # print(count) # for testing
+        if x == a:
+            print("Food : Calories : Fat : Carbs : Protein : Type \n")
+            pp.pprint(sheet.row_values(count))
+        else:
+            count += 1
+
+
+check_if_exist(user_input)
+
+"""
+pp = pprint.PrettyPrinter()
+ingredients = sheet.col_values(1)
+pp.pprint(ingredients)
+"""
